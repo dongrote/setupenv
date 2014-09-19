3ece814eabf0acbaa87426d5d3570ba5c54ebff1
Don Grote <dgrote@tacnetsol.com>
2014-09-19 11:38:41 -0400

    Added my PS1

--- setupenv.sh
+++ setupenv.sh
@@ -362,3 +362,6 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle;
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone git://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 echo "execute pathogen#infect()" >> ~/.vimrc
+
+# my PS1
+# PS1='\[\e[m\e[1;30m\][\j:\!\[\e[1;30m\]]\[\e[0;36m\] \t \d \[\e[1;30m\][\[\e[1;34m\]\u@\H\[\e[1;30m\]:\[\e[0;37m\] \[\e[0;32m\]+${SHLVL}\[\e[1;30m\]] \[\e[1;37m\]\w\[\e[38;5;2m\]${GIT_BRANCH}\[\e[0m\]\n$([ $? == 0 ] && echo -e "\e[0;32m:)\e[0m" || echo -e "\e[0;31m:(\e[0m") \$ '
